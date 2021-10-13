d325b16dfcbb1189875741161ab46f29f60b235e
sunmuffin42 <sunkulp.ananth@gmail.com>
2021-10-13 01:58:13 -0500

    add link target

---
+++ index-files/index.js
@@ -0,0 +1,13 @@
+links = document.getElementsByTagName("a")
+for (link of links) {
+    link.target = "_blank"
+}
+
+// links.forEach(link => {
+//     link.target = "_blank"
+// })
+// for (link of links) {
+//     link.target = "_blank"
+// }
+
+for (link of links) {link.target = "_blank"}
