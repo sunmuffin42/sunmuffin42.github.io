4329b1c5783b9d90ac1cdc1b8cf87ab6fecaa358
sunmuffin42 <sunkulp.ananth@gmail.com>
2022-10-25 04:29:49 -0400

    better nav bar, resources

--- index-files/index.js
+++ index-files/index.js
@@ -11,3 +11,20 @@
 // // }
 
 // for (link of links) {link.target = "_blank"}
+
+
+// for collapsible
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
